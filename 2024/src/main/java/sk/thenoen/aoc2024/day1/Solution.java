44e1bf14f4e3e7a74b04eff74faf5f02cf709392
thenoen <kordikmarek@gmail.com>
2024-12-01 10:16:10 +0100

    solved day1

--- 2024/src/main/java/sk/thenoen/aoc2024/day1/Solution.java
+++ 2024/src/main/java/sk/thenoen/aoc2024/day1/Solution.java
@@ -1,18 +1,74 @@
 package sk.thenoen.aoc2024.day1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LongSummaryStatistics;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import sk.thenoen.aoc.Utils;
 
 public class Solution {
 
+	public long solvePart1(String inputPath) {
+		final ArrayList<String> lines = Utils.loadLines(inputPath);
 
-	public long solvePart1() {
-		final ArrayList<String> lines = Utils.loadLines("day1/input.txt");
-		return lines.getFirst().length();
+		List<Integer> leftIds = new ArrayList<>();
+		List<Integer> rightIds = new ArrayList<>();
+
+		for (String line : lines) {
+
+			final Scanner scanner = new Scanner(line);
+			scanner.useDelimiter("   ");
+			leftIds.add(scanner.nextInt());
+			rightIds.add(scanner.nextInt());
+		}
+
+		leftIds.sort(Integer::compareTo);
+		rightIds.sort(Integer::compareTo);
+
+		long sum = 0;
+		for (int i = 0; i < lines.size(); i++) {
+			sum += Math.abs(leftIds.get(i) - rightIds.get(i));
+		}
+		return sum;
 	}
 
-	public long solvePart2(String inputPathString) {
-		return 0;
+	public long solvePart2(String inputPath) {
+		final ArrayList<String> lines = Utils.loadLines(inputPath);
+
+		List<Integer> leftIds = new ArrayList<>();
+		List<Integer> rightIds = new ArrayList<>();
+
+		for (String line : lines) {
+
+			final Scanner scanner = new Scanner(line);
+			scanner.useDelimiter("   ");
+			leftIds.add(scanner.nextInt());
+			rightIds.add(scanner.nextInt());
+		}
+
+		List<Similarity> similarities = new ArrayList<>();
+
+		leftIds.forEach(leftId -> {
+			final long count = rightIds.stream()
+									   .filter(leftId::equals)
+									   .count();
+			similarities.add(new Similarity(leftId, count));
+		});
+
+		final Long sum = similarities.stream()
+									 .map(similarity -> {
+										 return similarity.id * similarity.count;
+									 })
+									 .collect(Collectors.summingLong(Long::longValue));
+
+		return sum;
+	}
+
+	record Similarity(Integer id, Long count) {
+
 	}
 }
--- 2024/src/test/java/sk/thenone/aoc2024/day1/Day1Test.java
+++ 2024/src/test/java/sk/thenone/aoc2024/day1/Day1Test.java
@@ -8,12 +8,35 @@ import sk.thenoen.aoc2024.day1.Solution;
 public class Day1Test {
 
 	@Test
-	void part1() {
+	void part1Sample() {
 		Solution solution = new Solution();
-		long result = solution.solvePart1();
+		long result = solution.solvePart1("day1/sample.txt");
 		System.out.println("result: " + result);
-		Assertions.assertEquals(21, result);
+		Assertions.assertEquals(11, result);
 	}
 
+	@Test
+	void part1() {
+		Solution solution = new Solution();
+		long result = solution.solvePart1("day1/input.txt");
+		System.out.println("result: " + result);
+		Assertions.assertEquals(1320851, result);
+	}
+
+	@Test
+	void part2Sample() {
+		Solution solution = new Solution();
+		long result = solution.solvePart2("day1/sample.txt");
+		System.out.println("result: " + result);
+		Assertions.assertEquals(31, result);
+	}
+
+	@Test
+	void part2() {
+		Solution solution = new Solution();
+		long result = solution.solvePart2("day1/input.txt");
+		System.out.println("result: " + result);
+		Assertions.assertEquals(26859182, result);
+	}
 
 }
