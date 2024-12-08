ce7ec6f823df07317346707c55afcbb9dc03d9ca
marek <kordikmarek@gmail.com>
2024-12-08 14:17:25 +0100

    solved day8/part2

--- 2024/src/main/java/sk/thenoen/aoc2024/day8/SolutionPart2.java
+++ 2024/src/main/java/sk/thenoen/aoc2024/day8/SolutionPart2.java
@@ -2,9 +2,10 @@ package sk.thenoen.aoc2024.day8;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.Set;
 
 import sk.thenoen.aoc.Utils;
 
@@ -13,93 +14,160 @@ public class SolutionPart2 {
 	public long solve(String inputPath) {
 		final ArrayList<String> lines = Utils.loadLines(inputPath);
 
-		List<Problem> problems = new ArrayList<>();
-		Integer maxInputCount = 0;
+		char[][] map = new char[lines.size()][lines.getFirst().length()];
 
-		for (String line : lines) {
+		Map<Character, List<Position>> antennaTypes = new HashMap<>();
 
-			final String[] parts = line.split(":");
-			final long result = Long.parseLong(parts[0]);
-			final String[] inputStrings = parts[1].split(" ");
-
-			List<Long> inputs = new ArrayList<>();
-
-			for (int i = 1; i < inputStrings.length; i++) {
-				inputs.add(Long.parseLong(inputStrings[i]));
+		for (int x = 0; x < map.length; x++) {
+			for (int y = 0; y < map[0].length; y++) {
+				final char type = lines.get(x).charAt(y);
+				map[x][y] = type;
+				if (type == '.') {
+					continue;
+				}
+				antennaTypes.computeIfAbsent(type, k -> new ArrayList<>())
+							.add(new Position(x, y));
 			}
-
-			System.out.print(result);
-			System.out.print(" -> ");
-			System.out.println(inputs);
-
-			problems.add(new Problem(result, inputs));
-			if (maxInputCount < inputs.size()) {
-				maxInputCount = inputs.size();
-			}
-
 		}
 
-		Map<Integer, List<List<Operator>>> operatorCombinationsLengths = new HashMap<>();
-		final ArrayList<List<Operator>> value = new ArrayList<>();
-		value.add(new ArrayList<>());
-		operatorCombinationsLengths.put(0, value);
+		printMap(map);
 
-		for (Integer i = 1; i < maxInputCount; i++) {
-			List<List<Operator>> operatorCombinations = operatorCombinationsLengths.getOrDefault(i, operatorCombinationsLengths.get(i - 1));
-			List<List<Operator>> newOperatorCombinations = new ArrayList<>();
-			for (List<Operator> operatorCombination : operatorCombinations) {
-				for (Operator operator : Operator.values()) {
-					List<Operator> newOperatorCombination = new ArrayList<>(operatorCombination);
-					newOperatorCombination.add(operator);
-					newOperatorCombinations.add(newOperatorCombination);
+//		Set<Position> antinodes = new HashSet<>();
+		List<Position> antinodes = new ArrayList<>();
+
+		for (List<Position> antennas : antennaTypes.values()) {
+			for (int i = 0; i < antennas.size(); i++) {
+				for (int j = i + 1; j < antennas.size(); j++) {
+					System.out.println(i + " , " + j);
+					Position antennaA = antennas.get(i);
+					Position antennaB = antennas.get(j);
+
+					//					int[][] antinodesA = new int[map.length][2];
+					//					int[][] antinodesB = new int[map.length][2];
+					List<int[]> antinodesA = new ArrayList<>();
+					List<int[]> antinodesB = new ArrayList<>();
+
+					final int xDiff = Math.abs(antennaA.x - antennaB.x);
+					final int yDiff = Math.abs(antennaA.y - antennaB.y);
+
+					int maxXCount = map.length;
+					int maxYCount = map[0].length;
+					int maxCount = Math.max(maxXCount, maxYCount);
+
+					if (antennaA.x - antennaB.x < 0) { // A < B
+						// antinodeA[0] = antennaA.x - xDiff;
+						// antinodeB[0] = antennaB.x + xDiff;
+						int newAx = antennaA.x - xDiff;
+						while (newAx > -1 && newAx < map.length) {
+							antinodesA.add(new int[]{newAx, -1});
+							newAx -= xDiff;
+						}
+						int newBx = antennaB.x + xDiff;
+						while (newBx > -1 && newBx < map.length) {
+							antinodesB.add(new int[]{newBx, -1});
+							newBx += xDiff;
+						}
+
+					} else {
+						//						antinodeA[0] = antennaB.x - xDiff;
+						//						antinodeB[0] = antennaA.x + xDiff;
+						int newAx = antennaB.x - xDiff;
+						//						while (newAx > -1 && newAx < map.length) {
+						for (int iA = 0; iA < maxCount; iA++) {
+							antinodesA.add(new int[]{newAx, -1});
+							newAx -= xDiff;
+						}
+						int newBx = antennaA.x + xDiff;
+						//						while (newBx > -1 && newBx < map.length) {
+						for (int iB = 0; iB < maxCount; iB++) {
+							antinodesB.add(new int[]{newBx, -1});
+							newBx += xDiff;
+						}
+					}
+
+					if (antennaA.y - antennaB.y < 0) { // A > B
+						//						antinodeA[1] = antennaA.y - yDiff;
+						//						antinodeB[1] = antennaB.y + yDiff;
+						int newAy = antennaA.y - yDiff;
+						for (int iA = 0; iA < antinodesA.size(); iA++) {
+							antinodesA.get(iA)[1] = newAy;
+							newAy -= yDiff;
+						}
+						int newBy = antennaB.y + yDiff;
+						for (int iB = 0; iB < antinodesB.size(); iB++) {
+							antinodesB.get(iB)[1] = newBy;
+							newBy += yDiff;
+						}
+					} else {
+						//						antinodeA[1] = antennaA.y + yDiff;
+						//						antinodeB[1] = antennaB.y - yDiff;
+						int newAy = antennaA.y + yDiff;
+						for (int iA = 0; iA < antinodesA.size(); iA++) {
+							antinodesA.get(iA)[1] = newAy;
+							newAy += yDiff;
+						}
+						int newBy = antennaB.y - yDiff;
+						for (int iB = 0; iB < antinodesB.size(); iB++) {
+							antinodesB.get(iB)[1] = newBy;
+							newBy -= yDiff;
+						}
+					}
+
+					for (int[] antinodeA : antinodesA) {
+						final Position position = new Position(antinodeA[0], antinodeA[1]);
+						if (isInsideMap(position, map)) {
+							antinodes.add(position);
+						}
+					}
+
+					for (int[] antinodeB : antinodesB) {
+						final Position position = new Position(antinodeB[0], antinodeB[1]);
+						if (isInsideMap(position, map)) {
+							antinodes.add(position);
+						}
+					}
+
+					System.out.println("antinodes:" + antinodes.size());
 				}
+
 			}
-			operatorCombinationsLengths.put(i, newOperatorCombinations);
 		}
 
-		Long sumOfValidCalibrations = 0L;
-
-		for (Problem problem : problems) {
-
-			final List<List<Operator>> operatorCombinations = operatorCombinationsLengths.get(problem.inputs.size() - 1);
-
-			for (List<Operator> operatorCombination : operatorCombinations) {
-				long res = problem.inputs.get(0);
-				for (int i = 0; i < operatorCombination.size(); i++) {
-					res = operatorCombination.get(i).apply(res, problem.inputs.get(i + 1));
-				}
-				if (res == problem.result) {
-					sumOfValidCalibrations += res;
-					break;
-				}
-			}
-
+		for (Position antinode : antinodes) {
+			map[antinode.x][antinode.y] = '#';
 		}
 
-		return sumOfValidCalibrations;
+		System.out.println("------------------------");
+		printMap(map);
+
+		int count = 0;
+		for (int x = 0; x < map.length; x++) {
+			for (int y = 0; y < map[0].length; y++) {
+				if(map[x][y] != '.') {
+					count++;
+				}
+			}
+		}
+
+		return count;
 	}
 
-	private record Problem(Long result, List<Long> inputs) {
-
+	private static void printMap(char[][] puzzle) {
+		for (int x = 0; x < puzzle.length; x++) {
+			for (int y = 0; y < puzzle[0].length; y++) {
+				System.out.print(puzzle[x][y]);
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 
-	private enum Operator {
-		ADD((a, b) -> a + b),
-		MULTIPLY((a, b) -> a * b),
-		CONCAT((a, b) -> {
-			String aStr = a.toString();
-			String bStr = b.toString();
-			return Long.parseLong(aStr + bStr);
-		});
+	private static boolean isInsideMap(Position nextStep, char[][] map) {
+		return nextStep.x > -1 && nextStep.y > -1 &&
+			   nextStep.x < map.length && nextStep.y < map[0].length;
+	}
 
-		private BiFunction<Long, Long, Long> operation;
+	private record Position(int x, int y) {
 
-		Operator(BiFunction<Long, Long, Long> operation) {
-			this.operation = operation;
-		}
-
-		public Long apply(Long a, Long b) {
-			return this.operation.apply(a, b);
-		}
 	}
 }
--- 2024/src/test/java/sk/thenoen/aoc2024/day8/SolutionTest.java
+++ 2024/src/test/java/sk/thenoen/aoc2024/day8/SolutionTest.java
@@ -45,4 +45,43 @@ class SolutionTest {
 		System.out.println("result: " + result);
 		assertEquals(341, result);
 	}
+
+	/////////// PART 2 ///////////
+
+
+
+	@Test
+	void solvePart2Sample0() {
+		SolutionPart2 solution = new SolutionPart2();
+		long result = solution.solve("day8/samples/sample0.txt");
+		System.out.println("result: " + result);
+		assertEquals(30, result);
+	}
+
+
+	@Test
+	void solvePart2Sample2_1() {
+		SolutionPart2 solution = new SolutionPart2();
+		long result = solution.solve("day8/samples/sample2-1.txt");
+		System.out.println("result: " + result);
+		assertEquals(6, result);
+	}
+
+
+	@Test
+	void solvePart2Sample() {
+		SolutionPart2 solution = new SolutionPart2();
+		long result = solution.solve("day8/sample.txt");
+		System.out.println("result: " + result);
+		assertEquals(34, result);
+	}
+
+	@Test
+	void solvePart2() {
+		SolutionPart2 solution = new SolutionPart2();
+		long result = solution.solve("day8/input.txt");
+		System.out.println("result: " + result);
+		assertEquals(1134, result);
+	}
+
 }
