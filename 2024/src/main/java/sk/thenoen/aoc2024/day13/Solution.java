ca8f483152411fc4251cfec7c953e27fefc29f10
thenoen <kordikmarek@gmail.com>
2024-12-14 17:42:43 +0100

    solved day13 part 2 - preparation

--- 2024/src/main/java/sk/thenoen/aoc2024/day13/Solution.java
+++ 2024/src/main/java/sk/thenoen/aoc2024/day13/Solution.java
@@ -15,26 +15,45 @@ public class Solution {
 	private static final int A_COST = 3;
 	private static final int B_COST = 1;
 
-	public long solveAllPlantTypes(String inputPath) {
+	public long solvePart1(String inputPath) {
 		final ArrayList<String> lines = Utils.loadLines(inputPath);
 		final Pattern buttonPattern = Pattern.compile(BUTTON_REGEX);
 		final Pattern prizePattern = Pattern.compile(PRIZE_REGEX);
 
-		List<Problem> problems = new ArrayList<>();
-
-		parseInput(lines, buttonPattern, prizePattern, problems);
+		final List<Problem> problems = parseInput(lines, buttonPattern, prizePattern, 0);
 		long totalPrice = 0;
 
 		for (int i = 0; i < problems.size(); i++) {
 			System.out.println("problem: " + i);
-			final long price = solveProblem(problems.get(i));
+			final long price = solveProblem1(problems.get(i));
 			totalPrice += price;
 		}
 
 		return totalPrice;
 	}
 
-	private long solveProblem(Problem problem) {
+	public long solvePart2(String inputPath) {
+		final ArrayList<String> lines = Utils.loadLines(inputPath);
+		final Pattern buttonPattern = Pattern.compile(BUTTON_REGEX);
+		final Pattern prizePattern = Pattern.compile(PRIZE_REGEX);
+
+		List<Problem> problems = parseInput(lines, buttonPattern, prizePattern, 10_000_000_000_000L);
+
+		long totalPrice = 0;
+		for (int i = 0; i < problems.size(); i++) {
+			System.out.println("problem: " + i);
+			final long price = solveProblem2(problems.get(i));
+			if (price > 0) {
+				System.out.println("price: " + price);
+			}
+			totalPrice += price;
+			System.out.println();
+		}
+
+		return totalPrice;
+	}
+
+	private long solveProblem1(Problem problem) {
 
 		int minPrice = Integer.MAX_VALUE;
 		boolean hasSolution = false;
@@ -59,39 +78,75 @@ public class Solution {
 		return 0;
 	}
 
-	private static void parseInput(ArrayList<String> lines, Pattern buttonPattern, Pattern prizePattern, List<Problem> problems) {
+	private long solveProblem2(Problem problem) {
+
+		int minPrice = Integer.MAX_VALUE;
+		boolean hasSolution = false;
+
+		long maxA = problem.prize.x / problem.buttonA.x;
+		long maxB = problem.prize.x / problem.buttonB.y;
+
+		for (int a = 0; a < maxA; a++) {
+			for (int b = 0; b < maxB; b++) {
+
+				if (a * problem.buttonA.x + b * problem.buttonB.x == problem.prize.x &&
+					a * problem.buttonA.y + b * problem.buttonB.y == problem.prize.y) {
+					//					System.out.println("a: " + a + " b: " + b);
+					hasSolution = true;
+					int price = a * A_COST + b * B_COST;
+					if (price < minPrice) {
+						minPrice = price;
+					}
+				}
+			}
+		}
+		if (hasSolution) {
+			return minPrice;
+		}
+		return 0;
+	}
+
+	private static List<Problem> parseInput(ArrayList<String> lines,
+											Pattern buttonPattern,
+											Pattern prizePattern,
+											long conversionError) {
+		List<Problem> problems = new ArrayList<>();
 		for (int i = 0; i < lines.size(); i++) {
 			final Matcher matcherA = buttonPattern.matcher(lines.get(i));
 			matcherA.find();
 			final String aX = matcherA.group(1);
 			final String aY = matcherA.group(2);
-			Location buttonA = new Location(Integer.parseInt(aX), Integer.parseInt(aY));
+			Location buttonA = new Location(Long.parseLong(aX),
+											Long.parseLong(aY));
 			i++;
 			final Matcher matcherB = buttonPattern.matcher(lines.get(i));
 			matcherB.find();
 			final String bX = matcherB.group(1);
 			final String bY = matcherB.group(2);
-			Location buttonB = new Location(Integer.parseInt(bX), Integer.parseInt(bY));
+			Location buttonB = new Location(Long.parseLong(bX),
+											Long.parseLong(bY));
 			i++;
 
 			final Matcher prizeMatcher = prizePattern.matcher(lines.get(i));
 			prizeMatcher.find();
 			final String prizeX = prizeMatcher.group(1);
 			final String prizeY = prizeMatcher.group(2);
-			Location prize = new Location(Integer.parseInt(prizeX), Integer.parseInt(prizeY));
+			Location prize = new Location(Long.parseLong(prizeX)+ conversionError,
+										  Long.parseLong(prizeY)+ conversionError);
 
 			System.out.println();
 			i++;
 			Problem problem = new Problem(buttonA, buttonB, prize);
 			problems.add(problem);
 		}
+		return problems;
 	}
 
 	private record Problem(Location buttonA, Location buttonB, Location prize) {
 
 	}
 
-	private record Location(int x, int y) {
+	private record Location(long x, long y) {
 
 	}
 
--- 2024/src/test/java/sk/thenoen/aoc2024/day13/SolutionTest.java
+++ 2024/src/test/java/sk/thenoen/aoc2024/day13/SolutionTest.java
@@ -9,7 +9,7 @@ class SolutionTest {
 	@Test
 	void solvePart1Sample1() {
 		Solution solution = new Solution();
-		long result = solution.solveAllPlantTypes("day13/sample1.txt");
+		long result = solution.solvePart1("day13/sample1.txt");
 		System.out.println("result: " + result);
 		assertEquals(480, result);
 	}
@@ -17,8 +17,17 @@ class SolutionTest {
 	@Test
 	void solvePart1() {
 		Solution solution = new Solution();
-		long result = solution.solveAllPlantTypes("day13/input.txt");
+		long result = solution.solvePart1("day13/input.txt");
 		System.out.println("result: " + result);
 		assertEquals(36838, result);
+	}
+
+	@Test
+	void solvePart2Sample() {
+		//only problems 2 and 4 have solution
+		Solution solution = new Solution();
+		long result = solution.solvePart2("day13/sample1.txt");
+		System.out.println("result: " + result);
+		assertEquals(-1, result);
 	}
 }
